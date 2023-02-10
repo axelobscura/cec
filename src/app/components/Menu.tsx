4c17ef5dff9ec4dc47c7f1b79bbb4f2f098e8481
Axel <axosar@gmail.com>
2023-02-09 19:31:13 -0600

    acre

--- src/app/components/Menu.tsx
+++ src/app/components/Menu.tsx
@@ -1,4 +1,4 @@
-import { GiAbstract021, GiAbstract016 } from "react-icons/gi"
+import { SiFirebase, SiBroadcom, SiCastro, SiCkeditor4, SiChainlink, SiCrystal, SiDiscogs } from "react-icons/si";
 import ListGroup from 'react-bootstrap/ListGroup'
 import { useCategorias } from "lib/swr-hooks";
 
@@ -7,21 +7,28 @@ type MenuProps = {
 };
 
 export default function Menu({menuHover}:MenuProps) {
-
   const { categorias, isLoading } = useCategorias();
-
   if(isLoading){
     return(
       <h1>CARGANDO</h1>
     )
   }
-
+  const iconos = [
+    <SiFirebase size='1.5rem' />, 
+    <SiBroadcom size='1.5rem' />, 
+    <SiCastro size='1.5rem' />, 
+    <SiFirebase size='1.5rem' />,
+    <SiChainlink size='1.5rem' />,
+    <SiCkeditor4 size='1.5rem' />,
+    <SiCrystal size='1.5rem' />,
+    <SiDiscogs size='1.5rem' />
+  ]
   return (
     <ListGroup variant="flush">
       {categorias.map((val: any) => {
         return(
-          <ListGroup.Item key={val.id} data-nombre={val.nombre} onMouseEnter={(e) => menuHover(e)}>
-            <GiAbstract021 size='2em' />
+          <ListGroup.Item as="div" action key={val.id} data-nombre={val.nombre} onClick={(e) => menuHover(e)} className="m-1">
+            <>{iconos[val.id]}</>
           </ListGroup.Item>
         )
       })}
