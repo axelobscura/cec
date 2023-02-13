431273be3d1f565734724692f4341793411468d0
Axel <axosar@gmail.com>
2023-02-13 16:12:45 -0600

    acervo-viejo

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -4,7 +4,6 @@ export default function Head() {
       <title>ACERVO DIGITAL DEL CEMENTO Y DEL CONCRETO - Instituto Mexicano del Cemento y del Concreto A.C.</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Acervo digital sobre el cemento y el concreto del Instituto Mexicano del Cemento y del Concreto A.C., libros, investigación, Journal, Revistas y mucho más..." />
-      <link rel="icon" href="/favicon.ico" />
       <link rel="icon" type="image/png" href="/favicon.png"></link>
     </>
   )
