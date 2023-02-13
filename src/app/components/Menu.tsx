958fd0fb5424f4b97dcdcd2e96ccb7f6297599fe
Axel <axosar@gmail.com>
2023-02-13 14:38:47 -0600

    menu-icono

--- src/app/components/Menu.tsx
+++ src/app/components/Menu.tsx
@@ -1,4 +1,4 @@
-import { SiFirebase, SiBroadcom, SiCastro, SiCkeditor4, SiChainlink, SiCrystal, SiDiscogs } from "react-icons/si";
+import { SiFirebase, SiBroadcom, SiCastro, SiCkeditor4, SiChainlink, SiCrystal, SiDiscogs, SiAppveyor } from "react-icons/si";
 import { useCategorias } from "lib/swr-hooks";
 import Loader from "./Loader";
 
@@ -29,8 +29,18 @@ export default function Menu({menuHover}:MenuProps) {
       {categorias.map((val: any, idx: number) => {
         let Icon = icons[idx];
         return(
-          <li key={val.id} data-nombre={val.nombre} className="m-1">
-            <Icon data-nombre={val.nombre} onClick={(e:any) => menuHover(e)} size='1.9rem'>{val.nombre}</Icon>
+          <li key={val.id} className="m-1">
+            {
+              val.id === 1 ? <SiFirebase data-nombre={val.nombre} onClick={(e:any) => menuHover(e)} size='30px' /> : 
+              val.id === 2 ? <SiBroadcom data-nombre={val.nombre} onClick={(e:any) => menuHover(e)} size='30px' /> : 
+              val.id === 3 ? <SiCastro data-nombre={val.nombre} onClick={(e:any) => menuHover(e)} size='30px' /> : 
+              val.id === 4 ? <SiCkeditor4 data-nombre={val.nombre} onClick={(e:any) => menuHover(e)} size='30px' /> : 
+              val.id === 5 ? <SiChainlink data-nombre={val.nombre} onClick={(e:any) => menuHover(e)} size='30px' /> : 
+              val.id === 6 ? <SiCrystal data-nombre={val.nombre} onClick={(e:any) => menuHover(e)} size='30px' /> : 
+              val.id === 7 ? <SiDiscogs data-nombre={val.nombre} onClick={(e:any) => menuHover(e)} size='30px' /> : 
+              val.id === 8 ? <SiAppveyor data-nombre={val.nombre} onClick={(e:any) => menuHover(e)} size='30px' /> : 
+              <SiFirebase onClick={(e:any) => menuHover(e)} size='30px' />
+            }
           </li>
         )
       })}
