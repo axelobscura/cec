604595a99cf1855514a0d3fa2ad43b8d247cc231
axel <axosar@gmail.com>
2024-01-09 20:36:19 -0600

    text-color

--- src/pages/api/get-acervo.ts
+++ src/pages/api/get-acervo.ts
@@ -4,9 +4,11 @@ import { query } from '../../../lib/db'
 const handler: NextApiHandler = async (_, res) => {
   try {
     const results = await query(`
-      SELECT * FROM acervo
+      SELECT *
+      FROM acervo
       ORDER BY id ASC
-  `)
+      LIMIT 10;
+    `)
     return res.json(results)
   } catch (e: any) {
     res.status(500).json({ message: e.message })
