1f41600d0eec4f780ac085e13be8794d2cf42af5
axel <axosar@gmail.com>
2024-04-10 11:08:10 -0600

    contenido

--- src/app/documento/[titulo]/page.tsx
+++ src/app/documento/[titulo]/page.tsx
@@ -1,19 +1,25 @@
 "use client";
 import { useState } from 'react';
+import { usePathname } from 'next/navigation';
+import { useRouter } from 'next/navigation';
 import {Container, Row, Col} from 'react-bootstrap';
 import Footer from '../../components/Footer';
 
 export default function Documento() {
+  const router = useRouter();
+  const pathname = usePathname();
+
+  console.log(router);
   return (
     <>
       <Container fluid className='vh-100'>
         <Row>
-          <Col></Col>
-          <Col xs={12} md={6}>
-
+          <Col>
+            {pathname}
+          </Col>
+          <Col xs={12} md={8}>
             contenido
           </Col>
-
         </Row>
         <Footer/>
       </Container>
