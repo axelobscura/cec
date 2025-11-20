1fb4560c8d3c393caf059987ddb38bc49a533b62
Axel Laurent <axosar@gmail.com>
2025-11-20 10:12:03 -0600

    background

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,12 +1,12 @@
-import {Container, Row, Col} from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 import Buscador from './Buscador';
 import Image from 'next/image'
 
-export default function Footer(){
+export default function Footer() {
   return (
     <Row className='footer'>
       <Col>
-      <p className='text-gray-100'><small>® 1956 - 2024. Instituto Mexicano del Cemento y del Concreto A.C.</small></p>
+        <p className='text-gray-100'><small>® 1956 - 2025. Instituto Mexicano del Cemento y del Concreto A.C.</small></p>
       </Col>
     </Row>
   )
